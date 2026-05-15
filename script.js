2794f27588a046bc0af5d52535a395292374bb3b
Tuyisenge Esther <tuyisesther2020@gmail.com>
2026-05-15 15:40:48 +0200

    search function

--- script.js
+++ script.js
@@ -111,3 +111,46 @@ document.getElementById("eventForm").addEventListener("submit", function (e) {
 
     this.reset();
 });
+ document.getElementById("searchInput").addEventListener("input", function () {
+    let value = this.value.toLowerCase();
+
+    let filtered = events.filter(event =>
+        event.title.toLowerCase().includes(value) ||
+        event.category.toLowerCase().includes(value)
+    );
+
+    renderFilteredEvents(filtered);
+});
+
+function renderFilteredEvents(filteredEvents) {
+    let container = document.getElementById("eventContainer");
+    container.innerHTML = "";
+
+    filteredEvents.forEach(event => {
+
+            let card = document.createElement("div");
+        card.className = "bg-white p-4 rounded shadow";
+
+        card.innerHTML = `
+            <h2 class="font-bold text-lg">${event.title}</h2>
+            <p>Category: ${event.category}</p>
+            <p>Total Seats: ${event.seats}</p>
+            <p>Registered: ${event.registered}</p>
+            <p>Remaining: ${event.seats - event.registered}</p>
+
+            <button onclick="register(${event.id})"
+                class="bg-blue-500 text-white p-2 mt-2 rounded w-full">
+                Register
+            </button>
+
+            <button onclick="cancel(${event.id})"
+                class="bg-red-500 text-white p-2 mt-2 rounded w-full">
+                Cancel
+            </button>
+        `;
+
+        container.appendChild(card);
+    });
+
+    updateStats();
+}
