bc089927bc74269e56392136a1c2a859294958a5
Tuyisenge Esther <tuyisesther2020@gmail.com>
2026-05-15 13:56:18 +0200

    add initial data event

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let events = [
+    {
+        id: 1,
+        title: "AI Bootcamp",
+        category: "Technology",
+        seats: 30,
+        registered: 10
+    },
+    {
+        id: 2,
+        title: "Sports Day",
+        category: "Sports",
+        seats: 50,
+        registered: 20
+    }
+];
