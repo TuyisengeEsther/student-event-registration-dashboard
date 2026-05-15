a13a349a478267502b113da62a3561b6374834d3
Tuyisenge Esther <tuyisesther2020@gmail.com>
2026-05-15 14:11:10 +0200

    render event or dom manipulation

--- script.js
+++ script.js
@@ -28,3 +28,37 @@ function loadFromLocalStorage() {
 function saveToLocalStorage() {
     localStorage.setItem("events", JSON.stringify(events));
 }
+
+function renderEvents() {
+    let container = document.getElementById("eventContainer");
+    container.innerHTML = "";
+
+    events.forEach(event => {
+
+        let card = document.createElement("div");
+        card.className = "bg-white p-4 rounded shadow";
+
+        card.innerHTML = `
+            <h2 class="font-bold text-lg">${event.title}</h2>
+            <p>Category: ${event.category}</p>
+            <p>Total Seats: ${event.seats}</p>
+            <p>Registered: ${event.registered}</p>
+            <p>Remaining: ${event.seats - event.registered}</p>
+
+            <button onclick="register(${event.id})"
+                class="bg-blue-500 text-white p-2 mt-2 rounded w-full">
+                Register
+            </button>
+
+             <button onclick="cancel(${event.id})"
+                class="bg-red-500 text-white p-2 mt-2 rounded w-full">
+                Cancel
+            </button>
+        `;
+
+        container.appendChild(card);
+    });
+
+    updateStats();
+}
+
